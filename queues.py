d826e75145d6d1c952e0b4904edcf3de9bf10ebf
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-11 13:53:15 +0800

    Iteration of the FIFO class implemented. Added some comments of my learning for some important lines of code.

--- queues.py
+++ queues.py
@@ -1,8 +1,18 @@
 from collections import deque
 
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    # adding "*" to a parameter to have a varying number of positional argument and not just one
+    def __init__(self, *elements): 
+        self._elements = deque(elements)
+
+    # using a built-in function __len__ to make function len() operational
+    def __len__(self):
+        return len(self._elements)
+
+    # using a built-in function __iter__ to make the class iterable
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue() #yield is the same as return but it just pauses the execution of the function
 
     def enqueue(self, element):
         self._elements.append(element)
