e87304ac9940771760d24d9add49b74940a0129c
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-12 12:25:48 +0800

    Merged the testing file to a single file.

--- queues.py
+++ queues.py
@@ -19,3 +19,21 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
+
+# fifo = Queue()
+# fifo.enqueue("1st")
+# fifo.enqueue("2nd")
+# fifo.enqueue("3rd")
+
+# print(fifo.dequeue())
+# print(fifo.dequeue())
+# print(fifo.dequeue())
+
+fifo = Queue("1st", "2nd", "3rd")
+print(len(fifo))
+
+for element in fifo:
+    print(element)
+
+# this will return a vlaue of 0 after the iteration because of the built-in function __iter__
+print(len(fifo))
