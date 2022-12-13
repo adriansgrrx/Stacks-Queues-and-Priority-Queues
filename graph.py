a54adf6c731cbea3354e855a0d6840dceb15aec4
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-13 19:51:18 +0800

    Created a new graph instance and node mapping identifiers to city instances.

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
-from typing import NamedTuple 
+from typing import NamedTuple
+import networkx as nx
 
 class City(NamedTuple): # NamedTuples contain keys that are hashed to a particular value. 
     name: str           # it supports both access from key-value and iteration.
@@ -8,11 +9,23 @@ class City(NamedTuple): # NamedTuples contain keys that are hashed to a particul
     longitude: float
 
     @classmethod # @classmethod decorator is used to declare a method in the class as a class
-    def from_dict(cls, attrs): # from_dict() class method takes a dictionary of attributes extracted from a DOT file
+                    #⬇ cls implies that the method belongs to the class
+    def from_dict(cls, _attributes): # from_dict() class method takes a dictionary of attributes extracted from a DOT file
         return cls(
-            name=attrs["xlabel"],
-            country=attrs["country"],
-            year=int(attrs["year"]) or None,
-            latitude=float(attrs["latitude"]),
-            longitude=float(attrs["longitude"]),
-        ) # returns a new instance of your City class
+            name=_attributes["xlabel"],
+            country=_attributes["country"],
+            year=int(_attributes["year"]) or None,
+            latitude=float(_attributes["latitude"]),
+            longitude=float(_attributes["longitude"]),
+        ) # returns a new instance of the City class
+
+    def execute_graph(filename, node_factory):
+        graph = nx.nx_agraph.read_dot(filename)
+        nodes = {
+            name: node_factory(attributes)
+            for name, attributes in graph.nodes(data=True)
+        }
+        return nodes, nx.Graph(
+            (nodes[name1], nodes[name2], weights) # Each edge of a graph has an associated numerical value, called a weight.
+            for name1, name2, weights in graph.edges(data=True)
+        )
