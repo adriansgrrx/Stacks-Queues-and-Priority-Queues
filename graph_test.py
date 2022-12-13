8b4ca66394bbf1920b81fde72d4f430ddcdbbc84
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-13 21:06:54 +0800

    Breadth-First Search Using a FIFO Queue. Using built-in function, nx.bfs_tree().
    graph.py tester.

--- graph_test.py
+++ graph_test.py
@@ -1,6 +1,7 @@
+import networkx as nx
 from graph import City
 
-nodes, graph = City.load_graph("realpython_mat/roadmap.dot", City.from_dict)
+# nodes, graph = City.load_graph("realpython_mat/roadmap.dot", City.from_dict)
 
 # print(nodes["london"])
 # print(graph)
@@ -11,11 +12,26 @@ nodes, graph = City.load_graph("realpython_mat/roadmap.dot", City.from_dict)
 # for neighbor, weights in graph[nodes["london"]].items():
 #     print(weights["distance"], neighbor.name) # display weight edges
 
-def sort_by(neighbors, strategy):
-    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+# def sort_by(neighbors, strategy):
+#     return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
 
-def by_distance(weights):
-    return float(weights["distance"])
+# def by_distance(weights):
+#     return float(weights["distance"])
 
-for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
-    print(f"{weights['distance']:>3} miles, {neighbor.name}")
+# for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+#     print(f"{weights['distance']:>3} miles, {neighbor.name}")
+
+# **********************************************
+# Using built-in function, nx.bfs_tree() to know the certain year 
+# the city got its status with a certain year condition
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
+
+nodes, graph = City.load_graph("realpython_mat/roadmap.dot", City.from_dict)
+for node in nx.bfs_tree(graph, nodes["edinburgh"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
