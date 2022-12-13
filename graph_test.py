994d07334a56782199a6ed241d8859a22b835f13
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-13 20:49:56 +0800

    Displaying the weight edges, and accessing a node.
    graph.py tester.

--- graph_test.py
+++ graph_test.py
@@ -4,5 +4,9 @@ nodes, graph = City.load_graph("realpython_mat/roadmap.dot", City.from_dict)
 
 # print(nodes["london"])
 # print(graph)
-for neighbor in graph.neighbors(nodes["london"]): # built-in function .neighbors() syntax: Graph.neighbors(node)
-    print(neighbor.name)
+
+# for neighbor in graph.neighbors(nodes["london"]): # built-in function .neighbors() syntax: Graph.neighbors(node)
+#     print(neighbor.name)
+
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name) # display weight edges
