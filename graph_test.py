8ca6e316cc805c6387a02fcf79e2f00120fe2acd
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-13 20:52:33 +0800

    Sorting the algorithms into a function, and displayed using for loop.
    graph.py tester.

--- graph_test.py
+++ graph_test.py
@@ -8,5 +8,14 @@ nodes, graph = City.load_graph("realpython_mat/roadmap.dot", City.from_dict)
 # for neighbor in graph.neighbors(nodes["london"]): # built-in function .neighbors() syntax: Graph.neighbors(node)
 #     print(neighbor.name)
 
-for neighbor, weights in graph[nodes["london"]].items():
-    print(weights["distance"], neighbor.name) # display weight edges
+# for neighbor, weights in graph[nodes["london"]].items():
+#     print(weights["distance"], neighbor.name) # display weight edges
+
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(weights):
+    return float(weights["distance"])
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
