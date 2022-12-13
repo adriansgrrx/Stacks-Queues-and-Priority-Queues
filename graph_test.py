48023421af0d3683d59005746af0f744a1c3a12c
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-13 21:29:23 +0800

    Searching using Breadth-First Search for cities with a higher latitude, implemented.
    graph.py tester.

--- graph_test.py
+++ graph_test.py
@@ -27,8 +27,21 @@ from graph import City
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
+
 nodes, graph = City.load_graph("realpython_mat/roadmap.dot", City.from_dict)
-for node in nx.bfs_tree(graph, nodes["edinburgh"]):
+# for node in nx.bfs_tree(graph, nodes["edinburgh"]):
+#     print("📍", node.name)
+#     if is_twentieth_century(node.year):
+#         print("Found:", node.name, node.year)
+#         break
+# else:
+#     print("Not found")
+
+for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
     print("📍", node.name)
     if is_twentieth_century(node.year):
         print("Found:", node.name, node.year)
