e68608ffe6bb3e0aa497641973feda93931dde48
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-14 15:56:30 +0800

    Customized the main function, added several code blocks. Test: Animated Successfully.

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -3,7 +3,7 @@
 import argparse
 from queue import LifoQueue, PriorityQueue, Queue
 import threading
-from random import randint, choice
+from random import choice, randint
 from time import sleep
 from itertools import zip_longest
 from rich.align import Align
@@ -17,24 +17,6 @@ QUEUE_TYPES = {
     "lifo": LifoQueue,
     "heap": PriorityQueue
 }
-
-def main(args):
-    buffer = QUEUE_TYPES[args.queue]()
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
-    parser.add_argument("-p", "--producers", type=int, default=3)
-    parser.add_argument("-c", "--consumers", type=int, default=2)
-    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
-    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    try:
-        main(parse_args())
-    except KeyboardInterrupt:
-        pass
 
 # textual codes that Rich will eventually replace with the corresponding emoji glyphs.
 PRODUCTS = (
@@ -85,6 +67,26 @@ class Worker(threading.Thread):
         for _ in range(100):
             sleep(delay / 100)
             self.progress += 1
+
+class Producer(Worker):
+    def __init__(self, speed, buffer, products):
+        super().__init__(speed, buffer)
+        self.products = products
+
+    def run(self):
+        while True:
+            self.product = choice(self.products)
+            self.simulate_work()
+            self.buffer.put(self.product)
+            self.simulate_idle()
+
+class Consumer(Worker): # <-- Worker inherited
+    def run(self):
+        while True:
+            self.product = self.buffer.get()
+            self.simulate_work()
+            self.buffer.task_done()
+            self.simulate_idle()
 class View:
     def __init__(self, buffer, producers, consumers):
         self.buffer = buffer
@@ -132,22 +134,36 @@ class View:
         )
         return Panel(align, height=5, title=title)
 
-class Producer(Worker): # <-- Worker inherited
-    def __init__(self, speed, buffer, products):
-        super().__init__(speed, buffer)
-        self.products = products
+def main(args):
+    buffer = QUEUE_TYPES[args.queue]()
+    producers = [
+        Producer(args.producer_speed, buffer, PRODUCTS)
+        for _ in range(args.producers)
+    ]
+    consumers = [
+        Consumer(args.consumer_speed, buffer) for _ in range(args.consumers)
+    ]
 
-    def run(self):
-        while True:
-            self.product = choice(self.products)
-            self.simulate_work()
-            self.buffer.put(self.product)
-            self.simulate_idle()
+    for producer in producers:
+        producer.start()
 
-class Consumer(Worker): # <-- Worker inherited
-    def run(self):
-        while True:
-            self.product = self.buffer.get()
-            self.simulate_work()
-            self.buffer.task_done()
-            self.simulate_idle()
+    for consumer in consumers:
+        consumer.start()
+
+    view = View(buffer, producers, consumers)
+    view.animate()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
+    parser.add_argument("-p", "--producers", type=int, default=3)
+    parser.add_argument("-c", "--consumers", type=int, default=2)
+    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
+    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    try:
+        main(parse_args())
+    except KeyboardInterrupt:
+        pass
