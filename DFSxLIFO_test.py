ed32713cc561a66751353d81f36eb99f783f8cd5
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-12-14 13:03:14 +0800

    Traversal order changed, now it follows a linear path.

--- DFSxLIFO_test.py
+++ DFSxLIFO_test.py
@@ -1,7 +1,12 @@
 # Depth-First Search Using a LIFO Queue
 import networkx as nx
-from graph import City, load_graph
-
+from graph import (
+    City,
+    load_graph,
+    depth_first_traverse,
+    depth_first_search as dfs,
+)
+    
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
@@ -14,3 +19,15 @@ for node in nx.dfs_tree(graph, nodes["edinburgh"]):
         break
 else:
     print("Not found")
+
+print("\n****************************************************************************\n")
+
+def is_twentieth_century(city):
+    return city.year and 1901 <= city.year <= 2000
+
+nodes, graph = load_graph("realpython_mat/roadmap.dot", City.from_dict)
+city = dfs(graph, nodes["edinburgh"], is_twentieth_century)
+city.name
+
+for city in depth_first_traverse(graph, nodes["edinburgh"]):
+    print(city.name)
